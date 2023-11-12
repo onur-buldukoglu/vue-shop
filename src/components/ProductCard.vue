e96f034cb01989041233ecb4c30b0396030c6f45
Onur Buldukoglu <onur.buldukoglu@adesso.com.tr>
2023-11-12 21:04:43 +0300

    add addToCart handler

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
+import { cart } from "@/keys"
 import type { Product } from "@/types"
+import { inject } from "vue"
 defineProps<{ product: Product }>()
+const { addToCart } = inject(cart)!
 </script>
 
 <template>
@@ -11,7 +14,12 @@ defineProps<{ product: Product }>()
       <v-card-subtitle class="font-weight-bold">{{ product.category }}</v-card-subtitle>
       <v-card-text class="font-weight-bold mb-n13 text-h6">${{ product.price }}</v-card-text>
       <v-card-actions class="justify-end">
-        <v-btn class="bg-secondary text-accent text-capitalize font-weight-bold">Add to Cart</v-btn>
+        <v-btn
+          v-if="addToCart"
+          class="bg-secondary text-accent text-capitalize font-weight-bold"
+          @click="addToCart(product)"
+          >Add to Cart</v-btn
+        >
       </v-card-actions>
     </v-container>
   </v-card>
